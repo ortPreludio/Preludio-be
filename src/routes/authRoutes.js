f605bcc24ac9456357befc80ea4f810abefadbb6
IanGregorini <47628155@comunidadort.edu.ar>
2025-11-11 23:39:57 +0000

    Agrego ruta para perfil propio

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,14 @@
 import { Router } from 'express';
-import { register, login } from '../controllers/authController.js';
+import { register, login, logout } from '../controllers/authController.js';
+import { requireAuth } from "../middlewares/auth.js";
 
 export const authRouter = Router();
 authRouter.post('/register', register);
 authRouter.post('/login', login);
+authRouter.post('/logout', logout)
+authRouter.get('/me', requireAuth, async (req, res) => {
+  const User = (await import("../models/User.js")).default;
+  const u = await User.findById(req.user.id).lean();
+  if (!u) return res.status(404).json({ message: "No encontrado" });
+  res.json({ user: { id: u._id, nombre: u.nombre, apellido: u.apellido, email: u.email, rol: u.rol } });
+});
