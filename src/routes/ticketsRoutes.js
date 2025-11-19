46c83ff55aa51855057579fa448178984bffb95b
IanGregorini <47628155@comunidadort.edu.ar>
2025-11-19 20:52:27 +0000

    Handleo ticket id inexistente

--- src/routes/ticketsRoutes.js
+++ src/routes/ticketsRoutes.js
@@ -1,5 +1,6 @@
 // Rutas para Tickets
 import { Router } from 'express';
+import mongoose from "mongoose";
 import {
     createTicket,
     getMyTickets,
@@ -10,16 +11,24 @@ import {
 import { requireAuth, requireRole } from '../middlewares/auth.js';
 
 const router = Router();
+const ensureValidTicketId = (req, res, next) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    // Si no se encuentra el ID retornar 404 not found
+    return res.status(404).json({ message: "Ticket no encontrado" });
+  }
+  next();
+};
 
 // Crear ticket (usuario compra o admin crea)
 router.post('/', requireAuth, createTicket);
 // Ver mis tickets (o todos si es admin con ?todos=true)
 router.get('/', requireAuth, getMyTickets);
 // Ver un ticket específico
-router.get('/:id', requireAuth, getTicketById);
+router.get('/:id', requireAuth, ensureValidTicketId, getTicketById);
 // Cancelar/Eliminar ticket
-router.delete('/:id', requireAuth, deleteTicket);
+router.delete('/:id', requireAuth, requireRole('ADMIN'), ensureValidTicketId, deleteTicket);
 // Editar ticket (solo admin)
-router.put('/:id', requireAuth, requireRole('ADMIN'), updateTicket);
+router.put('/:id', requireAuth, requireRole('ADMIN'), ensureValidTicketId, updateTicket);
 
 export default router;
